be6c05ea88d18a56b4a2eb8cc48537e85087be37
Shaine123 <shaineberdida@gmail.com>
2026-04-12 19:20:31 +0800

    added logic for the item transfer

--- script.js
+++ script.js
@@ -2,16 +2,47 @@ const lists = document.querySelectorAll(".list");
 const cards = document.querySelectorAll(".card");
 
 for (const list of lists) {
-  list.addEventListener("dragstart", dragStart);
-  list.addEventListener("dragend", dragEnd);
+  list.addEventListener("dragenter", dragEnter);
+  list.addEventListener("dragleave", dragLeave);
+  list.addEventListener("drop", drop);
+  list.addEventListener("dragover", dragOver);
 }
 
 for (const card of cards) {
-  card.addEventListener("dragover", dragOver);
+  card.addEventListener("dragstart", dragStart);
+  card.addEventListener("dragend", dragEnd);
 }
 
 function dragStart(e) {
   const id = e.target.id;
 
+  console.log(id);
+
   e.dataTransfer.setData("text/plain", id);
+}
+
+function drop(e) {
+  e.preventDefault();
+
+  const data = e.dataTransfer.getData("text/plain");
+
+  const item = document.getElementById(data);
+
+  e.target.append(item);
+}
+
+function dragEnter(e) {
+  // console.log("test");
+}
+
+function dragEnd(e) {
+  // console.log("end");
+}
+
+function dragLeave(e) {
+  // console.log("practice");
+}
+
+function dragOver(e) {
+  e.preventDefault();
 }
